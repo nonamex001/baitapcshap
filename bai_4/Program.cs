ceeefb9a2098ed991bce8e5905dcb6df0559ba34
nonamex001 <quachbang34@gmail.com>
2026-05-06 14:12:41 +0700

    up

---
+++ bai_4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace bai_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so a : ");
+          int a = int.Parse(Console.ReadLine()!);
+            int [] arr = new int[a];
+            for (int i=0; i < a; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine()!);
+            }
+            for (int i=0; i < a; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+            int max = arr[0];
+            for (int i=0; i < a; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Gia tri lon nhat trong mang la: " + max);
+            
+        }
+    }
+}
